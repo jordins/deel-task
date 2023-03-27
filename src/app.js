429253e44a2865703559a13f51007b16001f350e
Jordi Nistal <jordi.nistal@gmail.com>
2023-03-27 22:09:40 +0200

    style: install eslint

--- src/app.js
+++ src/app.js
@@ -9,13 +9,11 @@ app.set("sequelize", sequelize);
 app.set("models", sequelize.models);
 
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get("/contracts/:id", getProfile, async (req, res) => {
   const { Contract } = req.app.get("models");
   const { id } = req.params;
-  // const contract = await Contract.findOne({where: {id}})
   const contract = await Contract.findOne({
     where: {
       [Op.and]: [
@@ -32,4 +30,5 @@ app.get("/contracts/:id", getProfile, async (req, res) => {
   if (!contract) return res.status(404).end();
   res.json(contract);
 });
+
 module.exports = app;
