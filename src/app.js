90e6f7fd060a5fea0ab6a6ac426ddfc16e888ef0
Jordi Nistal <jordi.nistal@gmail.com>
2023-03-27 22:25:15 +0200

    feat(contracts): add list non-terminated contracts

--- src/app.js
+++ src/app.js
@@ -31,4 +31,26 @@ app.get("/contracts/:id", getProfile, async (req, res) => {
   res.json(contract);
 });
 
+/**
+ * @returns a list of non-terminated contracts belonging to a user (client or contractor)
+ */
+app.get("/contracts", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const contracts = await Contract.findAll({
+    where: {
+      [Op.and]: [
+        { status: { [Op.ne]: "terminated" } },
+        {
+          [Op.or]: [
+            { ContractorId: req.profile.id },
+            { ClientId: req.profile.id },
+          ],
+        },
+      ],
+    },
+  });
+  if (!contracts || contracts.length === 0) return res.status(404).end();
+  res.json(contracts);
+});
+
 module.exports = app;
