538801b14b3250af6f9a89db31ed4bae321d858d
jordi <jordi@jordis-MBP.lan>
2023-03-27 16:18:44 +0200

    feat: get contracts by id
    fix api call to return the contract only if it belongs to the
    profile calling.

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
+const { Op } = require("sequelize");
+const app = express();
+app.use(bodyParser.json());
+app.set('sequelize', sequelize)
+app.set('models', sequelize.models)
+
+
+/**
+ * FIX ME!
+ * @returns contract by id
+ */
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const { id } = req.params
+    // const contract = await Contract.findOne({where: {id}})
+    const contract = await Contract.findOne({
+        where: {
+            [Op.and]: [
+                { id },
+                {
+                    [Op.or]: [
+                        { ContractorId: req.profile.id },
+                        { ClientId: req.profile.id },
+                    ]
+                }
+            ]
+        }
+    })
+    if (!contract) return res.status(404).end()
+    res.json(contract)
+})
+module.exports = app;
